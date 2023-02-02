e7c1ab3f1785682b72aff78eb3149d67733c2fff
Joel-100 <joelkugonza@gmail.com>
2023-02-02 13:11:38 +0300

    update README

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :postscounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  def most_recent_posts
+    posts.first(3)
+  end
+end
